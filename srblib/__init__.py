cfff8d5410687048e0a2b4d9dc241ccef83272c2
Sarbjit Singh <srbcheema1@gmail.com>
2018-12-12 16:43:23 +0530

    some cleaning

---
+++ srblib/__init__.py
@@ -0,0 +1,17 @@
+__version__ = '0.0.1'
+__mod_name__ = 'srblib'
+
+from .path import abs_path
+
+from .colour import Colour
+
+from .files import verify_file
+from .files import verify_folder
+from .files import remove
+
+'''
+return windows, linux or mac
+'''
+from .system import get_os_name
+
+from .util import show_dependency_error_and_exit
