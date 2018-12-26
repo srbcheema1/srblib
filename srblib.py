88a998f8085c7c5b56404da1d3e42497adaec432
Sarbjit Singh <srbcheema1@gmail.com>
2018-12-26 12:52:42 +0530

    heads to v0.0.2

--- srblib.py
+++ srblib.py
@@ -1,15 +1,18 @@
 from srblib import *
 
 if __name__ == "__main__":
-    dependency_map = {
-        'figlet':{
-            'ubuntu':'sudo apt remove figlet',
-        },
-    }
-    remove_dependencies(dependency_map,verbose=True)
-    dependency_map = {
-        'figlet':{
-            'ubuntu':'sudo apt install figlet',
-        },
-    }
+    from srblib.main import main
+    main()
+    # dependency_map = {
+        # 'figlet':{
+            # 'ubuntu':'sudo apt remove figlet',
+        # },
+    # }
+    # remove_dependencies(dependency_map,verbose=True)
+    # dependency_map = {
+        # 'figlet':{
+            # 'ubuntu':'sudo apt install figlet',
+        # },
+    # }
     # install_dependencies(dependency_map,verbose=True)
+
--- srblib/main.py
+++ srblib/main.py
@@ -1,3 +1,4 @@
+import sys
 
 try:
     import argparse
@@ -6,7 +7,18 @@ except:
     from .util import show_dependency_error_and_exit
     show_dependency_error_and_exit()
 
+from . import __mod_name__,__version__
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--version",
+                        action='store_true',
+                        help="Display version number")
+    autocomplete(parser)
+    parsed_args = parser.parse_args()
+    if(parsed_args.version):
+        print(__mod_name__+'=='+__version__)
+        sys.exit()
     return
 
 
