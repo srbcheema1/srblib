fd32cdf83581c62f71292faea76e5e21f58b596b
Sarbjit Singh <srbcheema1@gmail.com>
2018-12-26 18:01:00 +0530

    add strict tests for SrbJson

--- tests/srbjson_test.py
+++ tests/srbjson_test.py
@@ -17,3 +17,23 @@ def test_srbbank():
     assert(b['password'] == 'hello') # it gets updated automatically. while accesssing data using operators
     remove('hello')
     assert(b['debug'] == False) # it gets False value as the file was deleted and it is regenerated again
+
+def test_srbbank_strict():
+    if(not on_travis):
+        return
+    a = SrbJson('hello',{'test':{'debug':False,'password':None}},strict=True)
+    a['debug'] = True
+    a['password'] = 'helloworld'
+
+    try:
+        a['help'] = 'it will fail'
+        del a['debug']
+        assert(False) # it should not reach here
+    except:
+        pass
+
+    try:
+        b = SrbJson('hello',{'test':{'debug':False,'password':None,'good':False}},strict=True)
+        assert(False) # it should not reach here
+    except:
+        pass
