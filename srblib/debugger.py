a0c337d46b7e13815e53e451900ba21fda5ee57e
Sarbjit Singh <srbcheema1@gmail.com>
2018-12-30 16:16:29 +0530

    modifyiable on_travis

--- srblib/debugger.py
+++ srblib/debugger.py
@@ -5,10 +5,13 @@ on_srbpc = False
 if(os.environ['USER'] == 'srb'):
     on_srbpc = True
 
+_debug_template = {'srblib':{'debug':False,'on_travis':False}}
+_debug_json = SrbJson('~/.config/srblib/debug.json',_debug_template)
+
 on_travis = False
 if(os.environ['USER'] == 'travis'):
     on_travis = True
+if(not on_travis):
+    on_travis = _debug_json['on_travis']
 
-_debug_template = {'srblib':{'debug':False}}
-_debug_json = SrbJson('~/.config/srblib/debug.json',_debug_template)
 debug = _debug_json['debug']
